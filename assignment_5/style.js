fe99e015d90d06525691f3c742b7abe5ebdabcf4
rebecca <beccaramos@beccas-mbp.wireless.newschool.edu>
2018-12-04 18:24:45 -0500

    assignment 5 final

--- assignment_5/style.js
+++ assignment_5/style.js
@@ -1,29 +1,42 @@
 function change(){
-	var image = document.getElementById('img1');
-	image.src = "img1.gif"
-
-	var image = document.getElementById('img6');
-	image.src = "img6.gif"
-
-	var image = document.getElementById('img2');
-	image.src = "img2.gif"
-
 	var image = document.getElementById('img3');
 	image.src = "img3.gif"
-	
-	var image = document.getElementById('img4');
-	image.src = "img4.gif"
+}
 
+function changeOne(){
+	var image = document.getElementById('img1');
+	image.src = "img1.gif"
+}
+
+function changeTwo(){
+	var image = document.getElementById('img6');
+	image.src = "img6.gif"
+}
+function changeThree(){
 	var image = document.getElementById('img5');
 	image.src = "img5.gif"
-
+}
+function changeFour(){
 	var image = document.getElementById('img7');
 	image.src = "img7.gif"
+}
 
+function changeFive(){
+	var image = document.getElementById('img4');
+	image.src = "img4.gif"
+}
+
+function changeSix(){
 	var image = document.getElementById('img8');
-	image.src = "img8.gif"
+	image.src = "img8.gif"	
+}
 
+function changeSeven(){
 	var image = document.getElementById('img9');
 	image.src = "img9.gif"
+}
 
+function changeEight(){
+	var image = document.getElementById('img2');
+	image.src = "img2.gif"
 }
