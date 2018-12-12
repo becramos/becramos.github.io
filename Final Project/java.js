574225a0743510ad8270335840e9ebeeafee301b
rebecca <beccaramos@Beccas-MacBook-Pro.local>
2018-12-12 00:33:32 -0500

    push

---
+++ Final Project/java.js
@@ -0,0 +1,28 @@
+function changeOne(){
+	var image = document.getElementById('colin');
+	image.src = "colin.gif";
+}
+
+function changeTwo(){
+	var image = document.getElementById('lacey');
+	image.src = "lacey.gif"
+}
+
+function changeThree(){
+	var image = document.getElementById('odell');
+	image.src = "odell.gif"
+}
+
+function changeFour(){
+	var image = document.getElementById('lebron');
+	image.src = "lebron.gif"
+}
+function changeFive(){
+	var image = document.getElementById('serena');
+	image.src = "serena.gif"
+}
+function changeSix(){
+	var image = document.getElementById('shaquem');
+	image.src = "shaquem.gif"
+}
+
